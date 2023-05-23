2babc426215e8c303574a500a6b9f9a3f36d1455
KalfelsWiggers <gabriel_wiggers@estudante.sesisenai.org.br>
2023-05-23 09:16:45 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function Tocar(){
     audio2 = document.getElementById('audio2');
     audio2.play();
-    foguete.style.animation = "Subir 18s linear forwards"
+    foguete.style.animation = "Subir 18s ease-in-out forwards"
 }
